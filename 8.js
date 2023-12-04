83286dd4ba132d428460af4038ed86759940bced
VOLODYMYR KLADKO <kladkovn@gmail.com>
2023-12-04 11:18:31 +0200

    help please'

--- 8.js
+++ 8.js
@@ -1,12 +1,18 @@
 console.log("Завдання: 8 ==============================");
 
 function task8() {
-  // Створюємо функцію fetchFakeData, яка симулює запит до сервера та повертає "дані" через Promise.resolve().
-  // Створюємо fakeData константу з данними { name: "John",age: 30}
-  // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
-  // Викликаємо функцію fetchFakeData
-  // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
-  // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+  fetchFakeData(Promise.resolve())    // Створюємо функцію fetchFakeData, яка симулює запит до сервера та повертає "дані" через Promise.resolve().
+  const fakeData = { name: "John",age: 30};    // Створюємо fakeData константу з данними { name: "John",age: 30}
+  Promise.resolve((fakeData) => {
+    console.log(fakeData);
+  })    // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
+  fetchFakeData()    // Викликаємо функцію fetchFakeData
+  .then((values) => {
+    console.log(values);
+  })    // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
+  .catch((error) => {
+    console.log(('Error', error));
+  })    // Якщо проміс виконаний з помилкою виводимо в консоль помилку
 }
 
 // Викликаємо функцію task8
